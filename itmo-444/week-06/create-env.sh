e45cf1d7e69d40c0758c55e13f1f204f64dbaaea
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-10-03 19:48:48 +0000

    add lab progress

--- itmo-444/week-06/create-env.sh
+++ itmo-444/week-06/create-env.sh
@@ -17,11 +17,11 @@
 # Get Subnet 1 ID
 # Get Subnet 2 ID
 # Get VPCID
-VPCID=$(aws ec2 describe-vpcs --output=text --query='Vpcs[*].VpcId')
-SUBNETIDS1=$(aws ec2 describe-subnets --output=text --query 'Subnets[0].SubnetId')
-SUBNETIDS2=$(aws ec2 describe-subnets --output=text --query 'Subnets[1].SubnetId')
+VPCID=$(aws ec2 describe-vpcs --output=text --query='Vpcs[*].VpcId' --no-paginate)
+SUBNETIDS1=$(aws ec2 describe-subnets --output=text --query 'Subnets[0].SubnetId' --no-paginate)
+SUBNETIDS2=$(aws ec2 describe-subnets --output=text --query 'Subnets[1].SubnetId' --no-paginate)
 # Launch 3 EC2 instnaces 
-aws ec2 run-instances --image-id $1 --instance-type $2 --key-name $3 --security-group-ids $4 --count $5 --user-data file://install-env.sh
+aws ec2 run-instances --image-id $1 --instance-type $2 --key-name $3 --security-group-ids $4 --count $5 --user-data file://install-env.sh --no-paginate
 
 # Run EC2 wait until EC2 instances are in the running state
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/wait/index.html
@@ -32,24 +32,27 @@ aws elbv2 create-target-group \
 	        --protocol HTTP \
 		    --port 80 \
 		        --target-type instance \
-			    --vpc-id $VPCID
+			    --vpc-id $VPCID \
+			       --no-paginate
 			    
-TG=$(aws elbv2 describe-target-groups --output=text --query='TargetGroups[*].TargetGroupArn')
+TG=$(aws elbv2 describe-target-groups --output=text --query='TargetGroups[*].TargetGroupArn' --no-paginate )
 
 
 
 # create AWS elbv2 load-balancer
 aws elbv2 create-load-balancer \
 	    --name $7 \
-	        --subnets $SUBNETIDS1 $SUBNETIDS2
+	        --subnets $SUBNETIDS1 $SUBNETIDS2 \
+		   --no-paginate
 
 
 # AWS elbv2 wait for load-balancer available
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/load-balancer-available.html
 aws elbv2 wait load-balancer-available \
-	    --names $7
+	    --names $7 \
+	       --no-paginate
 
-LB=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerArn')
+LB=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerArn' --no-paginate)
 
 # create AWS elbv2 listener for HTTP on port 80
 #https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/create-listener.html
@@ -57,8 +60,9 @@ aws elbv2 create-listener \
 	    --load-balancer-arn $LB \
 	        --protocol HTTP \
 		    --port 80 \
-		        --default-actions Type=forward,TargetGroupArn=$TG
+		        --default-actions Type=forward,TargetGroupArn=$TG \
+			   --no-paginate
 
 # Retreive ELBv2 URL via aws elbv2 describe-load-balancers --query and print it to the screen
-URL=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].DNSName')
+URL=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].DNSName' --no-paginate)
 echo $URL
