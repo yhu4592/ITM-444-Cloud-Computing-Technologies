7e5c42448ac9bcce142bea6b300585087b7195d9
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-10-14 07:38:40 +0000

    add fixes to create script

--- itmo-444/week-06/create-env.sh
+++ itmo-444/week-06/create-env.sh
@@ -24,12 +24,14 @@ SUBNETIDS2=$(aws ec2 describe-subnets --output=text --query 'Subnets[1].SubnetId
 # Launch 3 EC2 instnaces 
 aws ec2 run-instances --image-id $1 --instance-type $2 --key-name $3 --security-group-ids $4 --count $5 --user-data file://install-env.sh --no-cli-pager
 
-IDS=$(aws ec2 describe-instances --filters Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].InstanceId")
+IDS=$(aws ec2 describe-instances --filters Name=instance-state-name,Values=pending --query "Reservations[*].Instances[*].InstanceId")
 
 # Run EC2 wait until EC2 instances are in the running state
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/wait/index.html
 aws ec2 wait instance-running \
 	    --instance-ids $IDS
+	    
+IDS=$(aws ec2 describe-instances --filters Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].InstanceId")
 
 # Create AWS elbv2 target group (use default values for health-checks)
 aws elbv2 create-target-group \
@@ -51,8 +53,8 @@ aws elbv2 wait target-in-service --target-group-arn $TG --no-cli-pager
 aws elbv2 create-load-balancer \
 	    --name $7 \
 	        --subnets $SUBNETIDS1 $SUBNETIDS2 \
-		      --no-cli-pager
-
+		      --no-cli-pager \
+                        --security-groups $4
 
 # AWS elbv2 wait for load-balancer available
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/load-balancer-available.html
