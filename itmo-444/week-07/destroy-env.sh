6401a9ec46c0d5ada6b2b730a55200e6fcd9b2ef
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-10-26 05:56:12 +0000

    add progress on destroying asg, commented out DB code

--- itmo-444/week-07/destroy-env.sh
+++ itmo-444/week-07/destroy-env.sh
@@ -11,34 +11,37 @@ LB=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].L
 TG=$(aws elbv2 describe-target-groups --output=text --query='TargetGroups[*].TargetGroupArn')
 LS=$(aws elbv2 describe-listeners --load-balancer-arn $LB --output=text --query='Listeners[*].ListenerArn')
 LCN=$(aws autoscaling describe-launch-configurations --output=text --query='LaunchConfigurations[*].LaunchConfigurationName')
-IDS=$(aws ec2 describe-instances --filters Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].InstanceId")
+IDS=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running,pending" --query "Reservations[*].Instances[*].InstanceId")
 DBIDS=$(aws rds describe-db-instances --output=text --query="DBInstances[*].DBInstanceIdentifier")
-
+echo "IDS: "
+echo $IDS
 aws autoscaling suspend-processes --auto-scaling-group-name $ASG
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name $ASG --min-size 0 --max-size 0
+aws autoscaling detach-instances --instance-ids $IDS --auto-scaling-group-name $ASG --should-decrement-desired-capacity
 
-#aws autoscaling detach-instances --instance-ids $IDS --auto-scaling-group-name $ASG --no-should-decrement-desired-capacity --no-cli-pager
-
+aws autoscaling detach-load-balancer-target-groups --auto-scaling-group-name $ASG --target-group-arns $TG
 
 aws ec2 terminate-instances --instance-ids $IDS 
 aws ec2 wait instance-terminated --instance-ids $IDS
 echo "Terminated Instances"
 
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $ASG
+echo "Deleted Auto Scaling Group"
+
+aws autoscaling delete-launch-configuration --launch-configuration-name $LCN
+echo "Deleted Launch Configuration"
 #aws autoscaling suspend-processes --auto-scaling-group-name $ASG
 
-aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $ASG 
-echo "Deleted Auto Scaling Group"
+
 
 aws elbv2 delete-listener --listener-arn $LS 
 echo "Deleted Listener"
 
-aws elbv2 delete-load-balancer --load-balancer-arn $LB 
-echo "Deleted Load Balancer"
-
-aws elbv2 delete-target-group --target-group-arn $TG 
+aws elbv2 delete-target-group --target-group-arn $TG
 echo "Deleted Target Group"
 
-aws autoscaling delete-launch-configuration --launch-configuration-name $LCN
-echo "Deleted Launch Configuration"
+aws elbv2 delete-load-balancer --load-balancer-arn $LB 
+echo "Deleted Load Balancer"
 
 for DBID in $DBIDS; do
 	
