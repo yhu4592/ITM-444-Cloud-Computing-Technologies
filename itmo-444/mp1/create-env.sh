00c556b5f6345ee1aa29a18ec7e7238e6122e8fa
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-11-10 23:09:05 -0600

    add recording 2 progress

--- itmo-444/mp1/create-env.sh
+++ itmo-444/mp1/create-env.sh
@@ -1,31 +1,133 @@
 #!/bin/bash
-#aws ec2 run-instances --image-id $1 --instance-type $2 --key-name $3 --security-group-ids $4 --user-data file://install-env.sh
-#aws ec2 run-instances --image-id $1 --instance-type $2 --key-name $3 --security-group-ids $4
 
-#create secret
-echo "creating secret..."
+######################################################################
+# Format of arguments.txt
+# $1 image-id
+# $2 instance-type
+# $3 key-name
+# $4 security-group-ids
+# $5 count (3)
+# $6 availability-zone
+# $7 elb name
+# $8 target group name
+# $9 auto-scaling group name
+# ${10} launch configuration name
+# ${11} db instance identifier (database name)
+# ${12} db instance identifier (for read-replica), append *-rpl*
+# ${13} min-size = 2
+# ${14} max-size = 5
+# ${15} desired-capacity = 3
+# ${16} Database engine (use mariadb)
+# ${17} Database name ( use company )
+# ${18} s3 raw bucket name (use initials and -raw)
+# ${19} s3 finished bucket name (use initials and -fin)
+# ${20} aws secret name
+# ${21} iam-instance-profile
+######################################################################
 
-# Add maria.json to your .gitignore file on your host system, push this to GitHub
-# Modify your maria-template.json not on the local system but on your Vagrant Box after you have
-# issued a git pull - rename the maria-template.json to maria.json and add a username and password
+######################################################################
+# New tasks to add
+# Note this is not in any order -- you need to think about this 
+# logically first -- perhaps write out on a piece of paper what you
+# want to happen - then try to code it
+######################################################################
+
+# Create auto-scaling group
+# Attach Launch configuration to auto-scaling group
+# Attach load-balancer to auto-scaling group
+# May have to change the logic of target group
+# Create RDS instance of mariadb (default values for now are fine)
+# Create and Read-Replica of RDS instance
+# Create all necessary wait commands
+
+# Tasks to accomplish
+
+# Get Subnet 1 ID
+# Get Subnet 2 ID
+SUBNET2A=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=
+availability-zone,Values=us-east-2a")
+SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=
+availability-zone,Values=us-east-2b")
+VPCID=$(aws ec2 describe-vpcs --output=text --query='Vpcs[*].VpcId')
+
+######################################################################################################################
+# New MP1 requirements
+######################################################################################################################
+# Modify the database launches to not have a public IP address - no pubic ip and conncet them to the same subnet as your EC2 instances so they can communicate securely
+# Remove your application layer security groups from the create-database, attach the two you set for ingress and egress
+
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/rds/create-db-instance.html
+# --publicly-accessible | --no-publicly-accessible (boolean)
+
+# Attach to a db-subnet-group
+# --db-subnet-group-name
+
+# Create a db-subnet-group and add your subnets to it
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/rds/create-db-subnet-group.html
+# --vpc-security-group-ids (list)
+
+#A list of Amazon EC2 VPC security groups to associate with this DB instance.
+#Attach two groups (see demo) -- one with ingress rules for 3306 and one with egress rules for 3306 and make the source your 
+
+# Create RDS subnet groups
+
+# Create IAM role
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/iam/create-role.html
+
+# Create IAM policy
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/iam/create-policy.html
+# https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_grammar.html
+
+######################################################################################################################
+# Week-07 template
+######################################################################################################################
+
+# Create Launch Configuration
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/autoscaling/create-launch-configuration.html
+
+aws autoscaling create-launch-configuration --launch-configuration-name ${10} --image-id $1 --instance-type $2 --key-name $3 --security-groups $4 --iam-instance-profile ${21} --user-data file://install-env.sh
+
+echo "Creating target group: $8"
+# Create AWS elbv2 target group (use default values for health-checks)
+TGARN=$(aws elbv2 create-target-group --name $8 --protocol HTTP --port 80 --target-type instance --vpc-id $VPCID --query="TargetGroups[*].TargetGroupArn")
+
+# create AWS elbv2 load-balancer
+echo "creating load balancer"
+ELBARN=$(aws elbv2 create-load-balancer --security-groups $4 --name $7 --subnets $SUBNET2A $SUBNET2B --query='LoadBalancers[*].LoadBalancerArn')
+
+# AWS elbv2 wait for load-balancer available
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/load-balancer-available.html
+echo "waiting for load balancer to be available"
+aws elbv2 wait load-balancer-available --load-balancer-arns $ELBARN
+echo "Load balancer available"
+
+# create AWS elbv2 listener for HTTP on port 80
+#https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/create-listener.html
+aws elbv2 create-listener --load-balancer-arn $ELBARN --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$TGARN
+
+# Create autoscaling group
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/autoscaling/create-auto-scaling-group.html
+
+echo "Launch configuration name is: ${10}"
+
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name ${9} --launch-configuration-name ${10} --min-size ${13} --max-size ${14} --desired-capacity ${15} --target-group-arns $TGARN  --health-check-type ELB --health-check-grace-period 600 --vpc-zone-identifier $SUBNET2A
+
+# Retreive ELBv2 URL via aws elbv2 describe-load-balancers --query and print it to the screen
+#https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/describe-load-balancers.html
+URL=$(aws elbv2 describe-load-balancers --output=json --load-balancer-arns $ELBARN --query='LoadBalancers[*].DNSName')
+echo $URL
+
+echo "creating secret"
 aws secretsmanager create-secret --name ${20} --secret-string file://maria.json
 
-# This is a shell script to scrape the username and password out of the secret string that is returned
-# This is pretty awful, but that is due to every variable is BASH being text only
-# This script works only if there are two values - username and password in the .json file
-# You would adjust the AWK print filed (note that value has nothing to do with the positional parameters)
 USERVALUE=$(aws secretsmanager get-secret-value --secret-id ${20} --output=json | jq '.SecretString' | tr -s , ' ' | tr -s ['"'] ' ' | awk {'print $6'} |  tr -d '\\')
-
 PASSVALUE=$(aws secretsmanager get-secret-value --secret-id ${20} --output=json | jq '.SecretString' | tr -s } ' ' | tr -s ['"'] ' ' | awk {'print $12'} | tr -d '\\')
 
-#create database
 echo "creating database"
 aws rds create-db-instance --db-instance-identifier ${11} --db-instance-class db.t3.micro --engine ${16} --master-username $USERVALUE --master-user-password $PASSVALUE --allocated-storage 20 --backup-retention-period 0
 
-# Note how in the wait command we can just use $11 instead of having to query for the db-instance-identifier
 aws rds wait db-instance-available --db-instance-identifier ${11}
 
-#create s3 bucket
 echo "creating s3 buckets"
 aws s3api create-bucket --bucket ${18} --region us-east-1
 aws s3api create-bucket --bucket ${19} --region us-east-1
@@ -34,8 +136,3 @@ aws s3api wait bucket-exists --bucket ${18}
 aws s3api wait bucket-exists --bucket ${19}
 echo "s3 buckets made"
 
-# use to build custom ec2 image
-# This will go into your create-launch-configuration but I am showing a stub here 
-# You will use ${21} in my sample video I hard coded the ARN - but you can use ${21} as you will pass in the name of the IAM
-# profile you created previously
-aws ec2 run-instances --image-id $1 --instance-type $2 --key-name $3 --security-group-ids $4 --iam-instance-profile ${21} --user-data file://install-env.sh
--- itmo-444/mp1/template-destroy-env.sh
+++ itmo-444/mp1/template-destroy-env.sh
@@ -3,14 +3,14 @@
 
 # Sample code to retrieve objects (file) names
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3api/list-objects-v2.html
-aws s3api list-objects-v2 --bucket ykh-itmo-raw --query 'Contents[*].Key'
-FILENAME=$(aws s3api list-objects-v2 --bucket jrh-itmo-raw --query 'Contents[*].Key')
+aws s3api list-objects-v2 --bucket ${18} --query 'Contents[*].Key'
+FILENAME=$(aws s3api list-objects-v2 --bucket ${18} --query 'Contents[*].Key')
 
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3api/delete-object.html
-aws s3api delete-object --bucket ykh-itmo-raw --key $FILENAME
+aws s3api delete-object --bucket ${18} --key $FILENAME
 
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3api/delete-bucket.html
-aws s3api delete-bucket --buket ykh-itmo-raw
+aws s3api delete-bucket --buket ${18}
 
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3api/list-buckets.html
 aws s3api list-buckets
