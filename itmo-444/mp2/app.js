a08938487002d9ae69345cd901cbb6fc73612771
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-12-04 21:52:17 -0600

    hardcode bucket

--- itmo-444/mp2/app.js
+++ itmo-444/mp2/app.js
@@ -25,31 +25,11 @@ const s3 = new S3Client({ region: REGION });
 // Using the AWS JavaScript SDK
 /////////////////////////////////////////////////////////////////////////// 
 
-//////////////////////////////////////////////////////////
-// Add S3 ListBucket code here
-//
-const listBuckets = async () => {
-
-	const client = new S3Client({region: "us-east-1" });
-        const command = new ListBucketsCommand({});
-	try {
-		const results = await client.send(command);
-		console.log("List Buckets Results: ", results.Buckets[0].Name);
-		const params = {
-			Bucket: results.Buckets[1].Name
-		}
-		return params;
-	
-} catch (err) {
-	console.error(err);
-}
-};
-
 var upload = multer({
     storage: multerS3({
         s3: s3,
         acl: 'public-read',
-        bucket: listBuckets().Bucket,
+        bucket: "ykh-raw",
         key: function (req, file, cb) {
             cb(null, file.originalname);
         }
@@ -201,7 +181,25 @@ const listAndCacheBuckets = async () => {
 	console.error(err);
 }
 };
+//////////////////////////////////////////////////////////
+// Add S3 ListBucket code here
+//
+const listBuckets = async () => {
 
+	const client = new S3Client({region: "us-east-1" });
+        const command = new ListBucketsCommand({});
+	try {
+		const results = await client.send(command);
+		console.log("List Buckets Results: ", results.Buckets[0].Name);
+		const params = {
+			Bucket: results.Buckets[1].Name
+		}
+		return params;
+	
+} catch (err) {
+	console.error(err);
+}
+};
 
 ///////////////////////////////////////
 // ListObjects S3 
