ce6d5fccfaf3f693adec05133c7f336ee97e5c8e
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-12-05 03:14:52 +0000

    fix listBuckets syntax

--- itmo-444/mp2/app.js
+++ itmo-444/mp2/app.js
@@ -29,7 +29,7 @@ var upload = multer({
     storage: multerS3({
         s3: s3,
         acl: 'public-read',
-        bucket: listBuckets(),
+        bucket: listBuckets().Bucket,
         key: function (req, file, cb) {
             cb(null, file.originalname);
         }
